c3f9f3ff5ce7d603dfaafe265b70eeac49165b58
Jaime De Leon Ferreira <jleon@icetex.gov.co>
2019-02-20 21:18:14 -0500

    Added Factorial

---
+++ EstudiosAlgoritmosCSharp/Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudiosAlgoritmosCSharp
+{
+    public class Factorial
+    {
+        public int GetFactorial(int n = 1)
+        {
+            int result = n;
+
+            if (n >= 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    result = result * i;
+                }
+            }
+
+            return result;
+        }
+        public int GetFactorialRecursivo(int n = 1)
+        {
+            if (n == 0) return 1;
+            else
+                return n * GetFactorial(n - 1);
+        }
+    }
+}
--- EstudiosAlgoritmosCSharp/Program.cs
+++ EstudiosAlgoritmosCSharp/Program.cs
@@ -9,9 +9,13 @@ namespace EstudiosAlgoritmosCSharp
             Console.WriteLine("Hello World!");
             NumerosPrimos np = new NumerosPrimos();
             Palindrome p = new Palindrome();
+            Factorial f = new Factorial();
+
 
             Console.WriteLine(np.EsPrimoRecursivo(11));
             Console.WriteLine(p.EsPalindromeRecursiva("dav ad"));
+            Console.WriteLine(f.GetFactorial(5));
+            Console.WriteLine(f.GetFactorialRecursivo(5));
             Console.ReadLine();
         }
     }
