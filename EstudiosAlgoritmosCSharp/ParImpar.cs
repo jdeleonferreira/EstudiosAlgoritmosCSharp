61b63b8915f096cb8d250f408bda7dd61d337fe2
Jaime De Leon Ferreira <jleon@icetex.gov.co>
2019-03-02 13:08:06 -0500

    Added Odd and Even numbers class

---
+++ EstudiosAlgoritmosCSharp/ParImpar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudiosAlgoritmosCSharp
+{
+    class ParImpar
+    {
+        public bool EsPar(int number)
+        {
+            bool result = false;
+
+            switch (number % 2)
+            {
+                case 0:
+                    result = true;
+                    Console.WriteLine("{0} is an even number", number);
+                    break;
+                case 1:
+                    result = false;
+                    Console.WriteLine("{0} is an odd number", number);
+                    break;
+            }
+
+            return result;
+
+        }
+
+        public string EsEvenOrOdd(int number)
+        {
+            string result = (number % 2 == 0) ? "Even" : "Odd";
+
+            return result;
+
+        }
+    }
+}
